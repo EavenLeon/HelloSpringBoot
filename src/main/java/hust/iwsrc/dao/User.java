2cd842f2b2aa10a2adbe5f3691ce905a11bcc932
liangyiwen <liangyiwen03@meituan.com>
2018-07-18 20:03:08 +0800

    format

--- src/main/java/hust/iwsrc/dao/User.java
+++ src/main/java/hust/iwsrc/dao/User.java
@@ -2,39 +2,46 @@ package hust.iwsrc.dao;
 
 import java.io.Serializable;
 
-/** 
+/**
  * @author: Yiwen Liang[ywhuak@163.com]
- * @date: Created on 18:42 2018/5/5 
- * @Description:  
+ * @date: Created on 18:42 2018/5/5
+ * @Description:
  */
 
-public class User implements Serializable{
+public class User implements Serializable {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	
-	private int id;
-	private String username;
-	
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String userName) {
-		this.username = userName;
-	}
-	
-	@Override
-	public String toString() {
-		// TODO Auto-generated method stub
-		return id+username;
-	}
-	
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+    private int id;
+    private String username;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setUsername(String userName) {
+        this.username = userName;
+
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        System.out.println();
+
+
+        return id + username;
+    }
+
 }
